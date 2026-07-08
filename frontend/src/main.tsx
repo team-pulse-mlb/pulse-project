2ee9558da83a5c1f7ad5ca9e703ce068ae95726b
KANG MINSEOK <to3644101@naver.com>
2026-07-08 16:11:32 +0900

    fix: 라우터 설정 복구

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
+
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
@@ -18,15 +18,3 @@ createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </StrictMode>,
 )
-=======
-import { RouterProvider } from 'react-router';
-
-import './shared/styles/global.css';
-import router from './app/router/root.tsx';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
->>>>>>> 119fe23499f2f79752f1a000ce522435b731b4ba
